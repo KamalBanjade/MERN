e70d1fcb9e7c91c58ae32d8a652f76da8142f933
Kamal Banjade <kamalbanjade67@gmail.com>
2024-03-23 14:33:35 +0545

    connected mongodb

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const db =
+"mongodb+srv://kamalbanjade:K%40M%40L@cluster0.sf0urdt.mongodb.net/books?retryWrites=true&w=majority&appName=Cluster0";
+/* Replace <password> with your database password */
+
+mongoose.set("strictQuery", true, "useNewUrlParser", true);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db);
+    console.log("MongoDB is Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
