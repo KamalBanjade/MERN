c0424f3e5999f5c8cc97859b956b71815c3e23d5
Kamal Banjade <kamalbanjade67@gmail.com>
2024-03-23 15:11:03 +0545

    reinitialized config and routes

--- server/app.js
+++ server/app.js
@@ -1,19 +1,26 @@
 const express = require('express');
-const connectDB = require('../config/db');
-const signinRoutes = require('./routes/api/signin'); // Import the sign-in routes
+const connectDB = require('./config/db');
+const routes = require('../routes/api/form'); // Import the sign-in routes
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+// use the cors middleware with the
+// origin and credentials options
+app.use(cors({ origin: true, credentials: true }));
+
+// use the body-parser middleware to parse JSON and URL-encoded data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// use the routes module as a middleware
+// for the /api/books path
+app.use("/api/books", routes);
 
 // Connect Database
 connectDB();
 
-// Middleware
-app.use(express.json()); // Parse JSON bodies
-
-// Routes
-app.get('/', (req, res) => res.send('Hello world!'));
-app.use('/api/signin', signinRoutes); // Use the sign-in routes at /api/signin
-
+app.get("/", (req, res) => res.send("Hello world!"));
 const port = process.env.PORT || 8082;
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
